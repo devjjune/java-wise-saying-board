f29c5254886e87458d6352d52f6127031cac563b
황지윤 <hjy648012@naver.com>
2026-01-28 14:15:45 +0900

    feat: 등록할 때마다 생성되는 명언번호 증가(4단계)

--- src/main/java/wiseSayingBoard/Application.java
+++ src/main/java/wiseSayingBoard/Application.java
@@ -6,6 +6,7 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
+        int order = 0;
 
         while (true) {
             System.out.println("명령) ");
@@ -14,13 +15,15 @@ public class Application {
             if (command.equals("종료")) {
                 break;
             } else if (command.equals("등록")) {
+                order ++;
+
                 System.out.println("명언 : ");
                 String wiseSaying = scanner.nextLine();
 
                 System.out.println("작가 : ");
                 String author = scanner.nextLine();
 
-                System.out.println("1번 명령이 등록되었습니다.");
+                System.out.println(order + "번 명령이 등록되었습니다.");
             }
         }
     }
