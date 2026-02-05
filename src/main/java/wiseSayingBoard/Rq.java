abdb0dbdcfc55cc83e8f1d4833de0b05577967c0
devjjune <jiyoon.hwang92@gmail.com>
2026-02-06 03:28:34 +0900

    refactor: Rq 클래스 예외 처리 및 다중 쿼리 분석 로직 추가

--- src/main/java/wiseSayingBoard/Rq.java
+++ src/main/java/wiseSayingBoard/Rq.java
@@ -12,13 +12,18 @@ public class Rq {
 
         String[] commandBits = command.split("\\?", 2);
         this.actionName = commandBits[0];
-        String queryString = commandBits[1]; // id=1
+        // ?가 없는 명령어일 경우 예외 처리
+        String queryString = commandBits.length > 1 ? commandBits[1] : "";
 
-        String[] queryBits = queryString.split("=", 2);
-        String key = queryBits[0]; // id
-        String value = queryBits[1]; // 1
+        // 다중 쿼리일 경우 분석 로직
+        String[] queryBits = queryString.split("&");
+        for (String queryBit : queryBits) {
+            String[] bits = queryBit.split("=", 2); // id=1
+            String key = bits[0]; // id
+            String value = bits.length > 1? bits[1] : ""; // 1
 
-        paramMap.put(key, value);
+            paramMap.put(key, value);
+        }
     }
 
     public String getActionName() {
